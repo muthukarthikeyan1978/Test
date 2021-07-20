a2ec8bb2a87490762bcb101fe7b9f07630b5139e
muthukarthikeyan1978 <muthukarthikeyan1978@gmail.com>
2021-07-20 22:03:52 +0800

    first commit

---
+++ src/main/java/com/sg/eamples/App.java
@@ -0,0 +1,13 @@
+package com.sg.eamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
